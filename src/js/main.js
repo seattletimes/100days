7b9ff5b1a9f3f5dd7e2deecec4474a3d126544be
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-01-28 13:54:31 -0800

    added/styled tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,21 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+require("./lib/qsa.js");
+
+var switchTab = function(e) {
+  if (e) e.preventDefault();
+  $(".switch-tab.activated").forEach(t => t.classList.remove("activated"));
+  this.classList.add("activated");
+  // var id = this.getAttribute("href");
+  // $(".players-by-line").forEach(t => t.classList.add("hidden"));
+  // var shown = document.querySelector(id);
+  // shown.classList.remove("hidden");
+
+  var first = shown.querySelector(".player");
+  var p = first.getAttribute("data-jersey");
+
+  showDetail(p);
+  selectPlayer.value = p;
+
+};
