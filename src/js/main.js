34d85d2a23d986e80bd66d619eba6b4e0908cbe3
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-04-26 13:40:39 -0700

    filters fixed; style tweaks
    Thank you, Thomas.

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,8 @@
 
 var $ = require("./lib/qsa.js");
 
+var section = $("[data-topic]");
 var planItems = $(".plan-item");
-var labelContainer = $(".label-container");
 var filterInputs = $(".plan-filter");
 var topicSelect = $.one("select.topics");
 
@@ -14,6 +14,8 @@ var changeFilter = function() {
   var checked = $(".plan-filter:checked");
   var categories = checked.map(check => check.id);
   if (!categories.length) categories = ["completed", "developing", "broken", "incomplete"];
+
+  console.log(categories);
   planItems.forEach(function(item) {
     var cat = item.getAttribute("data-progress");
     if (categories.indexOf(cat) > -1) {
@@ -28,12 +30,22 @@ var changeFilter = function() {
 var changeTopic = function() {
   //get topicSelect value
   var value = topicSelect.value;
-  if (!value) return;
   //loop through all non-hidden items
-  planItems.forEach(function(item) {
+  section.forEach(function(item) {
     var topic = item.getAttribute("data-topic");
-    if (topic !== value){
+    if (topic == value || !value){
+      item.classList.remove("hidden");
+    } else {
       item.classList.add("hidden");
+    }
+  })
+};
+
+var findEmpty = function() {
+  section.forEach(function(s) {
+    var visible = $(".plan-item:not(.hidden)", s)
+    if (!visible.length) {
+      s.classList.add("hidden");
     }
   })
 };
@@ -41,6 +53,7 @@ var changeTopic = function() {
 var applyFilters = function() {
   changeFilter();
   changeTopic();
+  findEmpty();
 }
 
 filterInputs.forEach(el => el.addEventListener("change", applyFilters));
@@ -48,15 +61,15 @@ topicSelect.addEventListener("change", applyFilters);
 applyFilters();
 
 //finding days remaining
-var moment = require("moment");
+// var moment = require("moment");
 
-var lastDay = moment("2017-04-29", "YYYY-MM-DD");
-var now = moment([]);
-var difference = lastDay.dayOfYear() - now.dayOfYear();
+// var lastDay = moment("2017-04-29", "YYYY-MM-DD");
+// var now = moment([]);
+// var difference = lastDay.dayOfYear() - now.dayOfYear();
 
-var firstDay = moment("2017-01-20", "YYYY-MM-DD");
-var currentDay = ( now.dayOfYear() - firstDay.dayOfYear() ) + 1;
+// var firstDay = moment("2017-01-20", "YYYY-MM-DD");
+// var currentDay = ( now.dayOfYear() - firstDay.dayOfYear() ) + 1;
 
 
-document.querySelector(".days-remaining").innerHTML = difference;
-document.querySelector(".currentDay").innerHTML = currentDay;
+// document.querySelector(".days-remaining").innerHTML = difference;
+// document.querySelector(".currentDay").innerHTML = currentDay;
